fca300a647714b8038e8b473670e8a7afd929ed1
ElAmirr <othmaniamir41gitgit config --global user.name ElAmirrgit config --global user.email othmaniamir41@gmail.com git config --global user.email othmaniamir41git>
2019-12-30 16:51:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Comment from './comments.js'
+import UsersList  from './userslist'
+import Posts from './posts.js'
+import {BrowserRouter, Route} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+const App = () =>  {
+  
+
+  
+
+  
+    return (
+      <BrowserRouter>
+      <div className="user-app">
+
+      <main className="user-app-main">
+            
+      <Route exact path='/' component={UsersList }/>            
+        
+      <Route exact path="/posts/:id" component={Posts} /> 
+      <Route   path='/comment/:id' component={Comment}/>
+        
+           
+      </main>
+      </div>
+      </BrowserRouter>
+
+      
+    );
+  
 }
+
 
 export default App;
